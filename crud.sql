069fa4f8b2a6c48a2c7bae428ca0cc47ba1a5d7a
Mário Lucas de Almeida Silva <mario.lucas250702@gmail.com>
2022-12-04 03:17:12 -0300

    feat: create tables.sql, selects.sql and start crud.sql

--- crud.sql
+++ crud.sql
@@ -4,10 +4,25 @@
 -- Criação
 
 -- 1)
+INSERT INTO clientes
+     (nome, lealdade)
+VALUES 
+    ('Georgia', 0);
 
 
 
 -- 2)
+INSERT INTO pedidos 
+    (id, status, cliente_id)
+VALUES 
+    (6,'Recebido', (
+SELECT 
+     id 
+FROM 
+     clientes 
+WHERE 
+     nome = 'Georgia')
+    );
 
 
 
@@ -17,7 +32,21 @@
 
 -- Leitura
 
--- 1)
+-- 1)  
+SELECT 
+    *
+FROM 
+    produtos_pedidos
+JOIN 
+    produtos ON produtos.id = produtos_pedidos.produto_id
+JOIN 
+    pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN 
+    clientes ON clientes.id = pedidos.cliente_id
+WHERE
+    clientes.nome ='Georgia';
+;
+  
 
 
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,64 @@
 -- Seleções de dados
 
 -- 1)
+SELECT 
+    *
+FROM 
+    produtos_pedidos
+JOIN
+    pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN 
+    produtos ON produtos.id = produtos_pedidos.produto_id;
 
 -- 2)
+SELECT 
+    pedidos.id
+FROM 
+    produtos_pedidos 
+JOIN
+    pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN 
+    produtos ON produtos.id = produtos_pedidos.produto_id
+WHERE
+    produtos.nome = 'Fritas';
+
 
 -- 3)
+SELECT 
+    clientes.nome
+FROM 
+    produtos_pedidos
+JOIN 
+    produtos ON produtos.id = produtos_pedidos.produto_id
+JOIN 
+    pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN 
+    clientes ON clientes.id = pedidos.cliente_id
+WHERE 
+    produtos.nome = 'Fritas';
 
 -- 4)
+SELECT 
+    SUM(produtos.preço)
+FROM 
+    produtos_pedidos
+JOIN 
+    produtos ON produtos.id = produtos_pedidos.produto_id
+JOIN 
+    pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN 
+    clientes ON clientes.id = pedidos.cliente_id
+WHERE 
+    clientes.nome = 'Laura';
 
 -- 5)
+SELECT 
+    produtos.nome,
+    COUNT (produto_id)  
+FROM 
+    produtos_pedidos 
+JOIN 
+    produtos ON produtos.id = produtos_pedidos.produto_id
+GROUP BY 
+    produtos.nome;
+   
